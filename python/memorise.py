36dbe5ac55cd9f76fba13a2d9522b35f785f62d2
Wes Mason <wesm@sully.isotomadev.com>
2009-09-23 09:20:21 +0100

    Started converting memorise to class decorator, bit faulty, needs work

--- python/memorise.py
+++ python/memorise.py
@@ -1,16 +1,25 @@
 import memcache
 from functools import wraps
 
-def memorise(fn, mc=None, mc_servers=None, set_key=None):
-        if not mc:
-                if not mc_servers:
-                        mc_servers = ['localhost:11211']
-                mc = memcache.Client(mc_servers, debug=0)
-        argnames = fn.func_code.co_varnames[:fn.func_code.co_argcount]
-        @wraps(fn)
-        def wrapper(*args, **kwargs):
-                def output_wrapper(value):
-                        return value
+class memorise(object):
+        def __init__(self, fn=None, mc=None, mc_servers=None, set_key=None):
+                self.fn = fn
+                self.set_key = set_key
+                if not mc:
+                        if not mc_servers:
+                                mc_servers = ['localhost:11211']
+                        self.mc = memcache.Client(mc_servers, debug=0)
+                else:
+                        self.mc = mc
+                        
+        def __call__(self, fn_self_or_fn=None, *args, **kwargs):
+                first_arg = None
+                if self.fn:
+                        fn = self.fn
+                        first_arg = fn_self_or_fn
+                else:
+                        fn = fn_self_or_fn
+                argnames = fn.func_code.co_varnames[:fn.func_code.co_argcount]
                 getter = False
                 method = False
                 if argnames[0] == 'self':
@@ -29,17 +38,21 @@ def memorise(fn, mc=None, mc_servers=None, set_key=None):
 
                 if method:
                         # Get the class name from the self argument
-                        parent_name = "%s::" % args[0].__class__.__name__
+                        try:
+                                fn_self = args[0]
+                        except IndexError:
+                                fn_self = first_arg
+                        parent_name = "%s::" % fn_self.__class__.__name__
                 else:
                         parent_name = ''
                 key = "%s%s(%s)" % (parent_name, fn.__name__, ",".join(arg_values_hash))
-                print key
-                if mc:
+                print "key: %s" % key
+                if self.mc:
                         if getter:
-                                output = mc.get(key)
+                                output = self.mc.get(key)
                                 if not output:
                                         output = fn(*args, **kwargs)
-                                        mc.set(key, output)
+                                        self.mc.set(key, output)
                                 if output.__class__ is memcache_none:
                                         output = None
                         else:
@@ -48,13 +61,13 @@ def memorise(fn, mc=None, mc_servers=None, set_key=None):
                                 else:
                                         offset = 0
                                 set_value = False
-                                if set_key:
+                                if self.set_key:
                                         if len(args) > offset:
-                                                arg_index = argnames.index(set_key)
+                                                arg_index = argnames.index(self.set_key)
                                                 set_value = args[arg_index]
                                         if set_value == False:
                                                 if len(kwargs) > 0:
-                                                        set_value = kwargs.get(set_key)
+                                                        set_value = kwargs.get(self.set_key)
                                 else:
                                         if len(args) > 1:
                                                 set_value = args[1]
@@ -66,12 +79,11 @@ def memorise(fn, mc=None, mc_servers=None, set_key=None):
                                         if set_value is None:
                                                 set_value = memcache_none()
                                 output = fn(*args, **kwargs)
-                                mc.set(key, set_value)
+                                self.mc.set(key, set_value)
 
                 else :
                         return fn(*args, **kwargs)
-                return output_wrapper(value=output)
-        return wrapper
+                return output
 
 class memcache_none:
         """Stub class for storing None values in memcache,
@@ -86,10 +98,10 @@ class Test:
                 self.t = None
 
         
+        @memorise(set_key='t')
         def set_t(self, t):
                 self.t = t
                 return True
-        set_t = memorise(set_t, set_key='t')
 
         @memorise
         def get_t(self):
