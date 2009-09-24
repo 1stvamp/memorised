a35b2b93b7f194db2d7fe359e2c60bfd800572fd
Wes Mason <wesm@sully.isotomadev.com>
2009-09-24 09:17:50 +0100

    Got memorise.py wrapper working properly with classes

--- python/memorise.py
+++ python/memorise.py
@@ -2,8 +2,7 @@ import memcache
 from functools import wraps
 
 class memorise(object):
-        def __init__(self, fn=None, mc=None, mc_servers=None, set_key=None):
-                self.fn = fn
+        def __init__(self, mc=None, mc_servers=None, set_key=None):
                 self.set_key = set_key
                 if not mc:
                         if not mc_servers:
@@ -12,78 +11,70 @@ class memorise(object):
                 else:
                         self.mc = mc
                         
-        def __call__(self, fn_self_or_fn=None, *args, **kwargs):
-                first_arg = None
-                if self.fn:
-                        fn = self.fn
-                        first_arg = fn_self_or_fn
-                else:
-                        fn = fn_self_or_fn
-                argnames = fn.func_code.co_varnames[:fn.func_code.co_argcount]
-                getter = False
-                method = False
-                if argnames[0] == 'self':
-                        method = True
-                        if len(args) == 1:
-                                getter = True
-                else:
-                        if len(args) == 0:
-                                getter = True
-
-                start = 0
-                arg_values_hash = []
-                for i,v in (zip(argnames, args) + kwargs.items()):
-                        if i != 'self':
-                                arg_values_hash.append("%s=%s" % (i,v))
-
-                if method:
-                        # Get the class name from the self argument
-                        try:
-                                fn_self = args[0]
-                        except IndexError:
-                                fn_self = first_arg
-                        parent_name = "%s::" % fn_self.__class__.__name__
-                else:
-                        parent_name = ''
-                key = "%s%s(%s)" % (parent_name, fn.__name__, ",".join(arg_values_hash))
-                print "key: %s" % key
-                if self.mc:
-                        if getter:
-                                output = self.mc.get(key)
-                                if not output:
-                                        output = fn(*args, **kwargs)
-                                        self.mc.set(key, output)
-                                if output.__class__ is memcache_none:
-                                        output = None
+        def __call__(self, fn):
+                @wraps(fn)
+                def wrapper(*args, **kwargs):
+                        argnames = fn.func_code.co_varnames[:fn.func_code.co_argcount]
+                        getter = False
+                        method = False
+                        if argnames[0] == 'self':
+                                method = True
+                                if len(args) == 1:
+                                        getter = True
                         else:
-                                if method:
-                                        offset = 1
+                                if len(args) == 0:
+                                        getter = True
+
+                        arg_values_hash = []
+                        for i,v in (zip(argnames, args) + kwargs.items()):
+                                if i != 'self':
+                                        arg_values_hash.append("%s=%s" % (i,v))
+
+                        if method:
+                                # Get the class name from the self argument
+                                parent_name = "%s::" % args[0].__class__.__name__
+                        else:
+                                parent_name = ''
+                        key = "%s%s(%s)" % (parent_name, fn.__name__, ",".join(arg_values_hash))
+                        print "key: %s" % key
+                        if self.mc:
+                                if getter:
+                                        output = self.mc.get(key)
+                                        if not output:
+                                                output = fn(*args, **kwargs)
+                                                self.mc.set(key, output)
+                                        if output.__class__ is memcache_none:
+                                                output = None
                                 else:
-                                        offset = 0
-                                set_value = False
-                                if self.set_key:
-                                        if len(args) > offset:
-                                                arg_index = argnames.index(self.set_key)
-                                                set_value = args[arg_index]
-                                        if set_value == False:
-                                                if len(kwargs) > 0:
-                                                        set_value = kwargs.get(self.set_key)
-                                else:
-                                        if len(args) > 1:
-                                                set_value = args[1]
+                                        if method:
+                                                offset = 1
                                         else:
-                                                if len(kwargs) > 0:
-                                                        set_value = kwargs.iteritems().pop(0)
+                                                offset = 0
+                                        set_value = False
+                                        if self.set_key:
+                                                if len(args) > offset:
+                                                        arg_index = argnames.index(self.set_key)
+                                                        set_value = args[arg_index]
+                                                if set_value == False:
+                                                        if len(kwargs) > 0:
+                                                                set_value = kwargs.get(self.set_key)
+                                        else:
+                                                if len(args) > 1:
+                                                        set_value = args[1]
+                                                else:
+                                                        if len(kwargs) > 0:
+                                                                set_value = kwargs.iteritems().pop(0)
 
-                                if set_value is not False:
-                                        if set_value is None:
-                                                set_value = memcache_none()
-                                output = fn(*args, **kwargs)
-                                self.mc.set(key, set_value)
+                                        if set_value is not False:
+                                                if set_value is None:
+                                                        set_value = memcache_none()
+                                        output = fn(*args, **kwargs)
+                                        self.mc.set(key, set_value)
 
-                else :
-                        return fn(*args, **kwargs)
-                return output
+                        else :
+                                return fn(*args, **kwargs)
+                        return output
+                return wrapper
 
 class memcache_none:
         """Stub class for storing None values in memcache,
@@ -103,11 +94,14 @@ class Test:
                 self.t = t
                 return True
 
-        @memorise
+        @memorise()
         def get_t(self):
                 return self.t
 
 
 test = Test()
 print test.set_t(t='test')
+print test.get_t()
+
+test = Test()
 print test.get_t()
