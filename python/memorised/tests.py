70b1ea1df018ee7b8dfe98b5009b54222074e17a
Wesley <1stvamp@gmail.com>
2009-09-26 01:41:51 +0100

    Starting sketching out unit tests for memorise

--- python/memorised/tests.py
+++ python/memorised/tests.py
@@ -1,27 +1,50 @@
 from memorised.decorators import memorise
+import unittest
+import memcache
+import random
 
-class Test:
+class TestModel:
         def __init__(self):
-                self.t = None
+                self.a = None
+		self.b = None
                 self.id = 1
 
 
-        def set_t(self, t):
-                self.t = t
-                return True
+        def set_a(self, a):
+                self.a = a
 
         @memorise(parent_keys=['id'])
-        def get_t(self):
-                return self.t
+        def get_a(self):
+                return self.a
+
+        def set_b(self, b):
+                self.b = b
+
+        def get_b(self):
+                return self.b
+
+@memorise()
+def func_get_a():
+	return random.uniform(0, 100)
+
+def func_get_b():
+	return random.uniform(0, 100)
+
+class TestMemorise(unittest.TestCase):
+	def setUp(self):
+		self.mc = memcache.Client(['localhost:11211'], debug=0)
+
+	def testsimplefunction(self):
+		pass
+
+	def testinstancemethod(self):
+		pass
+
+	def testclassmethod(self):
+		pass
 
 def run():
-        #test = Test()
-	#print "set_t(t='test'): %s" % test.set_t(t='test')
-	#print "get_t(): %s" % test.get_t()
-
-        test = Test()
-	print "get_t(): %s" % test.get_t()
-
+	unittest.main()
 
 if __name__ == '__main__':
         run()
