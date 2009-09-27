2b2dc302cf21e3063080833d0c4fb88b523fda5f
Wesley <1stvamp@gmail.com>
2009-09-27 21:36:19 +0100

    Fixing some whitespace issues

--- python/memorised/memorised/utils.py
+++ python/memorised/memorised/utils.py
@@ -15,16 +15,16 @@ def uncache(fn, mc=None, mc_servers=None, parent_keys=[]):
                 argnames = fn.func_code.co_varnames[:fn.func_code.co_argcount]
                 method = False
                 static = False
-		if hasattr(fn, 'im_self'):
-			method = True
-			if inspect.isclass(fn.im_self):
-				static = True
+                if hasattr(fn, 'im_self'):
+                        method = True
+                        if inspect.isclass(fn.im_self):
+                                static = True
 
                 arg_values_hash = []
                 # Grab all the keyworded and non-keyworded arguements so
                 # that we can use them in the hashed memcache key
                 for i,v in (zip(argnames, args) + kwargs.items()):
-			arg_values_hash.append("%s=%s" % (i,v))
+                        arg_values_hash.append("%s=%s" % (i,v))
 
                 class_name = None
                 if method:
@@ -32,13 +32,13 @@ def uncache(fn, mc=None, mc_servers=None, parent_keys=[]):
                         if len(parent_keys) > 0:
                                 for key in parent_keys:
                                         keys.append("%s=%s" % (key, getattr(fn.im_self, key)))
-			keys = ','.join(keys)
-			# Get the class name from the self argument
-			if inspect.isclass(fn.im_self):
-				class_name = fn.im_self.__name__
-			else:
-				class_name = fn.im_self.__class__.__name__
-			module_name = inspect.getmodule(fn.im_self).__name__
+                        keys = ','.join(keys)
+                        # Get the class name from the self argument
+                        if inspect.isclass(fn.im_self):
+                                class_name = fn.im_self.__name__
+                        else:
+                                class_name = fn.im_self.__class__.__name__
+                        module_name = inspect.getmodule(fn.im_self).__name__
                         parent_name = "%s.%s[%s]::" % (module_name, class_name, keys)
                 else:
                         # Function passed in, use the module name as the
--- python/memorised/setup.py
+++ python/memorised/setup.py
@@ -6,11 +6,11 @@ except ImportError:
     from setuptools import setup, find_packages
 
 setup(
-	name='memorised',
-	version='1.0',
-	author='Wes Mason',
-	author_email='wes [at] 1stvamp [dot] org',
-	url='http://github.com/1stvamp/oneversion/tree/master/python/memorised/',
-	packages=['memorised'],
-	install_requires=['python-memcached'],
+        name='memorised',
+        version='1.0',
+        author='Wes Mason',
+        author_email='wes [at] 1stvamp [dot] org',
+        url='http://github.com/1stvamp/oneversion/tree/master/python/memorised/',
+        packages=['memorised'],
+        install_requires=['python-memcached'],
 )
