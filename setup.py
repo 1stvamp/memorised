589a270fb1cf66478f14fd765d0c34fd9d07520e
Wesley Mason <wes@1stvamp.org>
2010-08-30 23:45:53 +0100

    Version bump + LICENSE update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
         name='memorised',
-        version='1.0',
+        version='1.0.1',
         author='Wes Mason',
         author_email='wes [at] 1stvamp [dot] org',
         url='http://github.com/1stvamp/oneversion/tree/master/python/memorised/',
