ddeefc2f3413e9fa5aa04668b34fd0e65dc6a85d
Wes Mason <wes@1stvamp.org>
2013-01-15 10:35:29 +0000

    Clean up setup and update license

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+"""Installer for memorised
+"""
+
+import os
+cwd = os.path.dirname(__file__)
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -10,7 +16,10 @@ setup(
         version='1.0.3',
         author='Wes Mason',
         author_email='wes@1stvamp.org',
+        description='memcache memoization decorators and utils for python',
+        long_description=open(os.path.join(cwd, 'README.rst')).read(),
         url='http://github.com/1stvamp//memorised/',
-        packages=['memorised'],
-        install_requires=['python-memcached'],
+        packages=find_packages(exclude=('ez_setup',)),
+        install_requires=('python-memcached',),
+        license='BSD'
 )
